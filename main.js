31a60448eb54e3a5d942cdcac7d87d375265d817
Andrea Chicco <andrea.chicco@flairbit.io>
2025-05-13 09:46:40 +0200

    update port

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express')
 require("dotenv").config()
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
